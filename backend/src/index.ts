e4aa12fdfacb631b38e5129be30d7328c6306018
Illu30qpPeHiK <bogutski2005@gmail.com>
2025-03-22 11:04:54 +0200

    Enhance blog post data structure with additional fields and update response format

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import blogRoutes from "./routes/blog.ts";
 import dotenv from "dotenv";
+import type { describe } from "node:test";
 
 dotenv.config();
 
@@ -22,11 +23,16 @@ app.get("/", (_, res) => {
   res.send("Сервер працює!");
 });
 
+// test data for blog posts
+
 const posts = [
-  { title: "Post 1", content: "Content of post 1" },
-  { title: "Post 2", content: "Content of post 2" }
+  { title: "Post 1", date: "2023-08-01", description: "Content of post 1" },
+  { title: "Post 2", date: "2023-08-02", description: "Content of post 2" },
+  { title: "Post 3", date: "2023-08-03", description: "Content of post 3" },
+  { title: "Post 4", date: "2023-08-04", description: "Content of post 4" },
+  { title: "Post 5", date: "2023-08-05", description: "Content of post 5" }
 ];
 
 app.get("/posts", (_, res) => {
-  res.json(posts.map(post => ({ name: post.title, content: post.content })));
+  res.json(posts.map(post => ({ title: post.title, date: post.date, description: post.description })));
 });
