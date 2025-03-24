c1115653fc94f83f7812b833bbb915009ef0609d
Illu30qpPeHiK <bogutski2005@gmail.com>
2025-03-24 23:34:52 +0200

    Specify entry file for SSR in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,11 @@ export default defineConfig(({ command, mode }): UserConfig => {
       tsconfigPaths(), 
       tailwindcss(),
     ],
+    build: {
+      rollupOptions: {
+        input: "src/entry.ssr.tsx", // Явно вказуємо вхідний файл
+      },
+    },
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
